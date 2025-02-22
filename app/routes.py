c219520ebe3ea23410b96ba383917b67a6a1cb58
Prestamo Estudiantes <prestamour@urosario.edu.co>
2025-02-22 11:28:46 -0500

    cambios para el lab1 hechos por la profe

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,6 @@ from flask import render_template, redirect, url_for, session
 from app import app
 from app.reading import read_db
 
-
-# app.secret_key = 'your_secret_key'
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -18,4 +15,13 @@ def register():
 @app.route('/login', methods=["GET"])
 def login():
     return render_template("login.html")
+
+
+@app.route('/edit_user/<email>', methods=['GET'])
+def edit_user(email):
+    db = read_db("db.txt")
+    user_info = db[email]
+    dni = user_info.get('dni')
+
+    return render_template('edit_user.html', user_data=user_info, email=email)
 
